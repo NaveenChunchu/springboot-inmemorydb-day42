8fd252bc9b421db5956e08c821f39eac2d492d4e
naveenchunchu <chunchunaveen2001@gmail.com>
2022-05-17 10:09:37 +0530

    completed...

---
+++ springboot-inmemorydb-day42/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into emp(ename,sal,deptno)values('KING',5000,10);
+insert into emp(ename,sal,deptno)values('JAMES',4398,13);
+insert into emp(ename,sal,deptno)values('WARD',3900,17);
+insert into emp(ename,sal,deptno)values('SMITH',5100,19);
+insert into emp(ename,sal,deptno)values('JONES',4200,11);
+insert into emp(ename,sal,deptno)values('MARTIN',6000,20);
+insert into emp(ename,sal,deptno)values('BLAKE',2100,34);
+
+
+
